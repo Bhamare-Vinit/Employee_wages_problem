6bb49c836dd8687a15c6410c97cb2e1bb51ba8dc
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-05 12:14:15 +0530

    [vinit] add: updated UC3_Part_time_Emp_wages

--- emp.py
+++ emp.py
@@ -1,13 +1,19 @@
 
 import random
 FULL_DAY_HOUR=8
+PART_TIME_HOUR=6
 WAGES_PER_HOUR=20
 def employee():
-    attendance=random.randint(0,1)
+    attendance=random.randint(0,1,2)
     if attendance == 0:
         print("Employee is present!")
         employee_wages=FULL_DAY_HOUR*WAGES_PER_HOUR
         print(f"Today's wage for the employee is: ${employee_wages:.2f}")
+    elif attendance==2:
+        print("Employee is Part time employee!")
+        employee_wages=PART_TIME_HOUR*WAGES_PER_HOUR
+        print(f"Today's wage for the employee is: ${employee_wages:.2f}")
+
     else:
         print("Employee is absent!")
 employee()
