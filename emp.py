afc51af3a94663152634a9e01feadd26aeee9590
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-05 12:41:19 +0530

    [vinit] add: updated UC4_find_daily_employee_wages_using_switchcase

--- emp.py
+++ emp.py
@@ -1,19 +1,31 @@
-
+'''
+@Author: Vinit Bhamare
+@Date: 07/05/2024 12:36:30
+@Last Modified by: vinit Bhamare
+@Last Modified time: 07/05/2024 12:36:30
+@Title : To find the Employee wages based on number of hours worked
+'''
 import random
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=6
 WAGES_PER_HOUR=20
 def employee():
-    attendance=random.randint(0,1,2)
-    if attendance == 0:
-        print("Employee is present!")
-        employee_wages=FULL_DAY_HOUR*WAGES_PER_HOUR
-        print(f"Today's wage for the employee is: ${employee_wages:.2f}")
-    elif attendance==2:
-        print("Employee is Part time employee!")
-        employee_wages=PART_TIME_HOUR*WAGES_PER_HOUR
-        print(f"Today's wage for the employee is: ${employee_wages:.2f}")
+    """
+    Description: function is used to find the dialy wages of employee
+    Parameter:None
+    Return:None
+    """
+    attendance=random.randint(0,2)
+    match attendance:
+        case 0:
+            print("Employee is present!")
+            employee_wages=FULL_DAY_HOUR*WAGES_PER_HOUR
+            print(f"Today's wage for the employee is: ${employee_wages:.2f}")
+        case 2:
+            print("Employee is Part time employee!")
+            employee_wages=PART_TIME_HOUR*WAGES_PER_HOUR
+            print(f"Today's wage for the employee is: ${employee_wages:.2f}")
 
-    else:
-        print("Employee is absent!")
+        case _:
+            print("Employee is absent!")
 employee()
