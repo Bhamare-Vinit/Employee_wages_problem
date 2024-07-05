2866800b4dc49e3b52ec73308760ddf51cf426c2
Bhamare-Vinit <vinitbhamare2002@gmail.com>
2024-07-05 13:40:25 +0530

    [vinit] add: updated UC5_Monthly_wages

--- emp.py
+++ emp.py
@@ -1,34 +1,36 @@
+import random
 '''
 @Author: Vinit Bhamare
 @Date: 07/05/2024 12:50:30
 @Title : To find the monthly wages of employees
 '''
-import random
-FULL_DAY_HOUR=8
-PART_TIME_HOUR=6
-WAGES_PER_HOUR=20
-WORKING_DAYS_IN_MONTH=26
-def employee():
+PER_HR_WAGE=20
+def employee_monthly_wage ():
     """
     Description: function is used to find the monthly wages of employees
     Parameter:None
     Return:None
     """
-    attendance=random.randint(0,2)
-    match attendance:
-        case 0:
-            print("Employee is present!")
-            employee_wages=FULL_DAY_HOUR*WAGES_PER_HOUR
-            print(f"Today's wage for the employee is: ${employee_wages:.2f}")
-            employee_wages_monthly=FULL_DAY_HOUR*WAGES_PER_HOUR*WORKING_DAYS_IN_MONTH
-            print(f"monthly wage for the employee is: ${employee_wages_monthly:.2f}")
-        case 2:
-            print("Employee is Part time employee!")
-            employee_wages=PART_TIME_HOUR*WAGES_PER_HOUR
-            print(f"Today's wage for the employee is: ${employee_wages:.2f}")
-            employee_wages_monthly=PART_TIME_HOUR*WAGES_PER_HOUR*WORKING_DAYS_IN_MONTH
-            print(f"monthly wage for the employee is: ${employee_wages_monthly:.2f}")
+    full_day = 0
+    part_time = 0
+    absent = 0
+    i = 1 
+    while i <= 20:
+        work = random.choice([0, 1, 2])
+        if work == 0:
+            absent += 1
+        elif work == 1:
+            full_day += 1
+        else:
+            part_time += 1
+        i += 1
+    full_day_wage = full_day * 8 * PER_HR_WAGE
+    half_day_wage = part_time * 4 * PER_HR_WAGE
+    absent_wage =  0
+    total_monthly_wage = full_day_wage + half_day_wage + absent_wage
+    
+    return print(f"The Mothly Wage of the employee is : {total_monthly_wage:.2f}")
 
-        case _:
-            print("Employee is absent!")
-employee()
+
+employee_monthly_wage()
+            
